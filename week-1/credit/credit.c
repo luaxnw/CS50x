71c59e3987ba9e84f2fb31514df47f3108fa3165
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-01-22 18:49:24 -0300

    Feito alterações

--- week-1/credit/credit.c
+++ week-1/credit/credit.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
 	long cardNumber = 4003600000000014;
 	long auxCardNumber = cardNumber;
 	int counter = 0;
+	int i = 0;
+	
 
-	while (auxCardNumber > 0){
+	while (auxCardNumber > 0)
+	{
 		auxCardNumber /= 10;
 		counter++;
 	}
 	printf("%i", counter);
+	int array[counter];
 
-        ## VISA = 13 or 16, MASTERCARD = 16, AMERICAN EXPRESS = 15
-	
+	switch (counter)
+	{
+	case 13:
+		while (cardNumber > 0)
+		{
+			i++;
+			int digito = cardNumber % 10;
+			printf("%i", digito);
+			cardNumber /= 10;
+			array[i] = digito;
+		}
+	}
 
+	/* VISA = 13 or 16, MASTERCARD = 16, AMERICAN EXPRESS = 15 */
 
 	return 0;
 }
