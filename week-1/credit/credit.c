5ed5c0f1bca099e0904aa192398152b0b1b5eeef
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-01-23 11:25:46 -0300

    Feito alterações

--- week-1/credit/credit.c
+++ week-1/credit/credit.c
@@ -1,36 +1,77 @@
 #include <stdio.h>
 
+int contadorInteiro(long x)
+{
+	int i = 0;
+	while (x > 0)
+	{
+		x /= 10;
+		i++;
+	}
+	return i;
+}
+
 int main()
 {
 
-	long cardNumber = 4003600000000014;
-	long auxCardNumber = cardNumber;
-	int counter = 0;
+	long cardNumber = 370360000000014;
 	int i = 0;
-	
-
-	while (auxCardNumber > 0)
-	{
-		auxCardNumber /= 10;
-		counter++;
-	}
-	printf("%i", counter);
+	int counter = contadorInteiro(cardNumber);
 	int array[counter];
+
+	while (cardNumber > 0)
+	{
+		i++;
+		array[i] = cardNumber % 10;
+		cardNumber /= 10;
+	}
+
+	for (int j = counter; j != 0; j--) // Printa em reverso
+	{
+		printf("%i ", array[j]);
+	}
 
 	switch (counter)
 	{
 	case 13:
-		while (cardNumber > 0)
+
+		if (array[12] == 4)
 		{
-			i++;
-			int digito = cardNumber % 10;
-			printf("%i", digito);
-			cardNumber /= 10;
-			array[i] = digito;
+			printf("\nVISA");
+			// Visa
 		}
+		break;
+
+	case 16:
+
+		if (array[counter] == 5 &&
+			(array[counter - 1] == 1 || array[counter - 1] == 2 || array[counter - 1] == 3 ||
+			 array[counter - 1] == 4 || array[counter - 1] == 5))
+
+		{
+			printf("\nMASTERCARD");
+			// Mastercard
+		}
+
+		if (array[counter] == 4)
+		{
+			printf("\nVISA");
+			// Visa
+		}
+		break;
+	case 15:
+
+		if (array[counter] == 3 && array[counter - 1] == 7)
+		{
+			printf("\nAMERICAN EXPRESS");
+			// American Express
+		}
+		break;
 	}
 
-	/* VISA = 13 or 16, MASTERCARD = 16, AMERICAN EXPRESS = 15 */
+	/* VISA = 13 or 16, MASTERCARD = 16, AMERICAN EXPRESS = 15
+	American Express 34 or 37, MasterCard = 51, 52, 53, 54, or 55
+	Visa numbers start with 4 */
 
 	return 0;
 }
