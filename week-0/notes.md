e0fdab6fb0f9b49e95dbb7687d8b936c112d4a38
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-01-30 19:07:20 -0300

    Finaliza scrabble.

--- week-0/notes.md
+++ week-0/notes.md
@@ -1,9 +1,23 @@
 # Semana 0 - Scratch
 
-anotações
-
 ## Binários
 
-Em 1 byte temos do bit 0 ao bit 7 [0,7], portando são 8 bits. Esse, ao passado para decimal, gera o valor de 255 [0, 255], sendo assim 256 combinações diferentes.  
-Combinações do quê? Em ASCII, cada uma representa um caractere (então cada caractere ocupa 1 byte), em RGB cada uma representa uma intensidade de cor. Então são valores decimais são abstratos e servem para codificação computacional.
+Em 1 byte temos do bit 0 ao bit 7 [0,7], portando são 8 bits. Esse, ao passado para decimal, gera o valor de 255 [0, 255], sendo assim 256 combinações diferentes. Esse valor de 256 sendo o máximo para cada byte:
+        00000001 e 00000010 são ambos bytes, porém com ordens com nível lógico 1 distintas. Sendo o primeiro lido em decimal como 1 e o segundo como 2. Ambos são bytes com uma única combinação de bits, mas com o bit em nível lógico 1 em posições diferentes, o que resulta em valores decimais distintos (1 e 2).
 
+Combinações do quê? Exemplo:
+- Em ASCII, cada 1 valor dos 256 representa um caractere;
+- em RGB cada 1 valor representa uma intensidade de cor.
+
+Portanto pode representar coisas diferentes dependendo do contexto, sendo que cada byte possui 256 formas de representação.
+
+## Comparação 
+
+Comparação rápida com base 10 (decimal)
+Cada posição é um potência de 10 dependendo de sua ordem. Como:
+345 --> 3.10² + 4.10¹ + 5.10⁰
+
+### O mesmo ocorre com binários
+
+Sendo na base 2, cada posição é uma potência de 2 e depende de sua ordem:
+1001 --> 1.2³ + 0.2² + 0.2¹ + 1.2⁰ = 9 (em decimal)
--- week-2/scrabble/scrabble.c
+++ week-2/scrabble/scrabble.c
@@ -3,21 +3,86 @@
 
 int main()
 {
-
-    int table[26];
-    char palavra[100];
-    scanf("%99s", palavra);
-    int len = strlen(palavra);
+    int pointsTable[26] = {
+        1,  // a
+        3,  // b
+        3,  // c
+        2,  // d
+        1,  // e
+        4,  // f
+        2,  // g
+        4,  // h
+        1,  // i
+        8,  // j
+        5,  // k
+        1,  // l
+        3,  // m
+        1,  // n
+        1,  // o
+        3,  // p
+        10, // q
+        1,  // r
+        1,  // s
+        1,  // t
+        1,  // u
+        4,  // v
+        4,  // w
+        8,  // x
+        4,  // y
+        10  // z
+    };
+    int points1 = 0;
+    int points2 = 0;
+    char tableAscii[26];
 
     for (int i = 0; i < 26; i++)
     {
-        table[i] = 97 + i;
+        tableAscii[i] = 'a' + i; //cria o alfabeto a partir de 'a' + i. chegando à z
     }
 
-    for (int j = 0; j < 26; j++)
+    char word1[100];
+    printf("Player 1 --> ");
+    scanf("%99s", word1);
+    int len1 = strlen(word1);
 
+    for (int i = 0; i < len1; i++) //faz a leitura de letra da palavra e compara com o alfabeto
     {
-        printf("%i ", table[j]);
+        for (int j = 0; j < 26; j++)
+        {
+            if (word1[i] == tableAscii[j])
+            {
+                points1 += pointsTable[j]; //se a letra corresponde ao alfabeto, faz a adição conforme a pointsTable
+            }
+        }
+    }
+
+    char word2[100];
+    printf("Player 2 --> ");
+    scanf("%99s", word2);
+    int len2 = strlen(word2);
+
+    for (int i = 0; i < len2; i++) //faz a leitura de letra da palavra e compara com o alfabeto
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            if (word2[i] == tableAscii[j])
+            {
+                points2 += pointsTable[j]; //se a letra corresponde ao alfabeto, faz a adição conforme a pointsTable
+            }
+        }
+    }
+
+    if (points1 > points2)
+    {
+        printf("Player 1 wins with %i points against %i! ", points1, points2);
+    }
+    else if (points1 < points2)
+    {
+        printf("Player 2 wins with %i points against %i! ", points2, points1);
+    }
+    else if (points1 == points2)
+    {
+        printf("\nThats a tie!\nPlayer 1 --> %i\nPlayer 2 --> %i", points1, points2);
     }
 
     return 0;
